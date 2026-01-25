b103db5303a58d297b7d20c64cf315934ef7991a
Kartik Garg <kartikamitgarg2005@gmail.com>
2026-01-25 14:24:34 +0530

    Added the main.py file

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from newsroom-agent!")
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+from crewai import Crew, Process
+from agents import create_agents
+from tasks import create_tasks
 
-if __name__ == "__main__":
-    main()
+topic = "Universal Basic Income"
+
+blue_pundit, red_pundit, editor = create_agents()
+tasks = create_tasks(blue_pundit, red_pundit, editor, topic)
+
+newsroom_crew = Crew(
+    agents=[blue_pundit, red_pundit, editor],
+    tasks=tasks,
+    process=Process.hierarchical,
+    manager_llm="gpt-4o",
+    verbose=True
+)
+
+result = newsroom_crew.kickoff()
+print(result)
